a16bdf101648f2ad9ee001602a6c1711d9df6093
Mozart <156059535+Mozart01pplg2@users.noreply.github.com>
2025-07-14 16:05:13 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-echo "<h2>Topup App berhasil jalan!</h2>";
-echo "<a href='/app/customer/index.php'>Masuk Customer</a>";
-
 require '../includes/db.php';
 $produk = $pdo->query("SELECT * FROM produk WHERE aktif = TRUE")->fetchAll();
 ?>
